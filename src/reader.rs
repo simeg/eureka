065b6ef83960ddc856d1f71356f5ceb189c514d9
Simon Egersand <s.egersand@gmail.com>
2018-12-27 22:02:23 +0100

    Trim all user input
    After refactoring something a new-line character was added onto the
    input from the user.

--- src/reader.rs
+++ src/reader.rs
@@ -12,7 +12,7 @@ impl<R: io::BufRead> Read for Reader<R> {
     fn read(&mut self) -> String {
         let mut input = String::new();
         self.reader.read_line(&mut input).unwrap();
-        input
+        input.trim().to_string()
     }
 }
 
