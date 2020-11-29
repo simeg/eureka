7539153c6f22eb63557bbc66211c83a800e31157
Simon Egersand <segersand@spotify.com>
2020-11-29 19:28:03 +0100

    Test ProgramAccess

--- src/program_access.rs
+++ src/program_access.rs
@@ -1,7 +1,7 @@
 use std::io::ErrorKind;
 use std::path::PathBuf;
 use std::process::Command;
-use std::{env, io};
+use std::{env, fs, io};
 
 pub trait ProgramOpener {
     fn open_editor(&self, file_path: &str) -> io::Result<()>;
@@ -32,10 +32,71 @@ impl ProgramAccess {
             .map(PathBuf::from)
             .or_else(|_| self.get_if_available(fallback))?;
 
+        // Make sure file exists
+        fs::metadata(&file_path)?;
         Command::new(&program).arg(&file_path).status().map(|_| ())
     }
 
     fn get_if_available(&self, program: &str) -> io::Result<PathBuf> {
         which::which(program).map_err(|err| std::io::Error::new(ErrorKind::NotFound, err))
+    }
+}
+
+#[allow(non_snake_case)]
+#[cfg(test)]
+mod tests {
+    use crate::program_access::ProgramAccess;
+    use std::env;
+
+    type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+    #[test]
+    fn test_program_access__get_if_available__success() {
+        let program_access = ProgramAccess {};
+
+        let actual = program_access.get_if_available("echo");
+
+        assert!(actual.is_ok())
+    }
+
+    #[test]
+    fn test_program_access__get_if_available__failure() {
+        let program_access = ProgramAccess {};
+
+        let actual = program_access.get_if_available("some-non-existing-program");
+
+        assert!(actual.is_err())
+    }
+
+    #[test]
+    fn test_program_access__open_with_fallback() -> TestResult {
+        let program_access = ProgramAccess {};
+        let tmp_file = tempfile::NamedTempFile::new()?;
+        let file_path = tmp_file.path().to_str().unwrap();
+        env::set_var("READER_ENV_VAR", "echo");
+
+        let actual = program_access.open_with_fallback(
+            file_path,
+            "READER_ENV_VAR",
+            "some-non-existing-program",
+        );
+
+        env::remove_var("READER_ENV_VARIABLE");
+
+        assert!(actual.is_ok());
+        Ok(())
+    }
+
+    #[test]
+    fn test_program_access__open_with_fallback__uses_fallback() -> TestResult {
+        let program_access = ProgramAccess {};
+        let tmp_file = tempfile::NamedTempFile::new()?;
+        let file_path = tmp_file.path().to_str().unwrap();
+        env::remove_var("THIS_ENV_VAR");
+
+        let actual = program_access.open_with_fallback(file_path, "THIS_ENV_VAR", "echo");
+
+        assert!(actual.is_ok());
+        Ok(())
     }
 }
