1fc47d4014d68eb8d57e6250e8cfb27d3af5fd34
Simon Egersand <segersand@spotify.com>
2021-07-24 19:11:14 +0200

    Add tests for git module

--- src/git.rs
+++ src/git.rs
@@ -44,11 +44,10 @@ impl GitManagement for Git {
         }
 
         let refname = format!("refs/heads/{}", branch_name);
-        let obj = repo.revparse_single(&*refname)?;
+        let obj = repo.revparse_single(refname.as_str())?;
 
         repo.checkout_tree(&obj, None)?;
-
-        repo.set_head(&*refname)
+        repo.set_head(refname.as_str())
     }
 
     fn add(&self) -> Result<(), git2::Error> {
@@ -145,4 +144,130 @@ where
 
         Err(git2::Error::from_str("No authentication method succeeded"))
     })
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod tests {
+    use crate::git::{find_last_commit, Git, GitManagement};
+    use git2::{BranchType, Repository, RepositoryInitOptions, Status};
+    use tempfile::{NamedTempFile, TempDir};
+
+    #[test]
+    fn test_git__init__valid_repo() {
+        let mut git = Git::default();
+        // Valid repo
+        let (dir, _repo, _file) = repo_init();
+
+        let actual = git.init(dir.path().to_str().unwrap());
+
+        assert!(actual.is_ok());
+    }
+
+    #[test]
+    fn test_git__init__invalid_repo() {
+        let mut git = Git::default();
+        // Invalid repo
+        let dir = TempDir::new().unwrap();
+
+        let actual = git.init(dir.path().to_str().unwrap());
+
+        assert!(actual.is_err());
+    }
+
+    #[test]
+    fn test_git__checkout_branch__missing_branch() {
+        let mut git = Git::default();
+        let (dir, repo, _file) = repo_init();
+        git.init(dir.path().to_str().unwrap()).unwrap();
+
+        // This will create a new branch
+        git.checkout_branch("new-branch-name").unwrap();
+
+        let actual = repo.find_branch("new-branch-name", BranchType::Local);
+
+        assert!(actual.is_ok());
+    }
+
+    #[test]
+    fn test_git__checkout_branch__success() {
+        let mut git = Git::default();
+        let (dir, repo, _file) = repo_init();
+        git.init(dir.path().to_str().unwrap()).unwrap();
+
+        let before = repo.head();
+        assert_eq!(before.unwrap().name().unwrap(), "refs/heads/main");
+
+        git.checkout_branch("new-branch-name").unwrap();
+
+        let after = repo.head();
+
+        assert!(after.is_ok());
+        assert_eq!(after.unwrap().name().unwrap(), "refs/heads/new-branch-name");
+    }
+
+    #[test]
+    fn test_git__add__success() {
+        let mut git = Git::default();
+        let (dir, repo, _file) = repo_init();
+        git.init(dir.path().to_str().unwrap()).unwrap();
+
+        let statuses_before = repo.statuses(None).unwrap();
+        let before = statuses_before.get(0).unwrap();
+        assert_eq!(before.status(), Status::WT_NEW);
+
+        git.add().unwrap();
+
+        let statuses_after = repo.statuses(None).unwrap();
+        let after = statuses_after.get(0).unwrap();
+        assert_eq!(after.status(), Status::INDEX_NEW);
+    }
+
+    #[test]
+    fn test_git__commit__success() {
+        let mut git = Git::default();
+        let (dir, _repo, _file) = repo_init();
+        git.init(dir.path().to_str().unwrap()).unwrap();
+
+        // Initial commit
+        let before = find_last_commit(git.repo.as_ref().unwrap());
+        assert_eq!(before.unwrap().summary().unwrap(), "initial-msg");
+
+        git.add().unwrap();
+        git.commit("some-subject").unwrap();
+
+        let after = find_last_commit(git.repo.as_ref().unwrap());
+        assert_eq!(after.unwrap().summary().unwrap(), "some-subject");
+    }
+
+    fn repo_init() -> (TempDir, Repository, NamedTempFile) {
+        let td = TempDir::new().unwrap();
+        let mut opts = RepositoryInitOptions::new();
+        opts.initial_head("main");
+        let repo = Repository::init_opts(td.path(), &opts).unwrap();
+
+        // Create README.md file
+        let file = tempfile::Builder::new()
+            .prefix("README")
+            .suffix(".md")
+            .rand_bytes(0)
+            .tempfile_in(td.path())
+            .unwrap();
+        {
+            // Set basic config
+            let mut config = repo.config().unwrap();
+            config.set_str("user.name", "some-name").unwrap();
+            config.set_str("user.email", "some-email").unwrap();
+
+            // Make initial commit
+            let mut index = repo.index().unwrap();
+            let id = index.write_tree().unwrap();
+            let tree = repo.find_tree(id).unwrap();
+            let sig = repo.signature().unwrap();
+            repo.commit(Some("HEAD"), &sig, &sig, "initial-msg", &tree, &[])
+                .unwrap();
+        }
+        // Return file to not drop it and make it disappear
+        (td, repo, file)
+    }
 }
